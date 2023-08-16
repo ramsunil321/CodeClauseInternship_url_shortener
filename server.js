37ccea444d4a16fc84225a661439687b70ac07f1
Ram Sunil <gramsunil321@gmail.com>
2023-08-16 19:20:02 +0530

    Updating shortenAPI

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const axios = require('axios'); // Import axios
 const ShortUrl = require('./models/shortUrl');
-const shortid = require('shortid'); 
+const shortid = require('shortid');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +16,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-
 const generateShortUrlKey = () => {
   return shortid.generate();
 };
@@ -26,8 +26,16 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/shortUrls', async (req, res) => {
-  const shortUrlKey = generateShortUrlKey();
-  await ShortUrl.create({ fullUrl: req.body.fullUrl, shortUrl: shortUrlKey });
+  const inputValue = req.body.fullUrl;
+
+  try {
+    const response = await axios(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(inputValue)}`);
+    const shortUrl = response.data.result.full_short_link;
+    await ShortUrl.create({ fullUrl: inputValue, shortUrl });
+  } catch (error) {
+    console.error('Error creating short URL:', error);
+  }
+
   res.redirect('/');
 });
 
@@ -42,14 +50,14 @@ app.get('/:shortUrl', async (req, res) => {
 });
 
 app.delete('/delete/:id', async (req, res) => {
-    try {
-      await ShortUrl.findByIdAndDelete(req.params.id);
-      res.sendStatus(200);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  });
+  try {
+    await ShortUrl.findByIdAndDelete(req.params.id);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
